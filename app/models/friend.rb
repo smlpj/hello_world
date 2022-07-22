44b7064dc98897c3a375e559d5383f76f174c75b
Samuel <slopeza@uninorte.edu.co>
2022-07-22 09:46:29 -0500

    friends.rb corrected code

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,5 +1,5 @@
 class Friend < ApplicationRecord
-    validates :first_name, :last_name, :email, :phone, :twitter, presence: true
+    validates :first_name, :last_name, presence: true
     validates :email, format: { with: /[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+/, message: "%{value} is not a valid email" }
     validates :phone, numericality: true
     validates :twitter, format: { with: /@(\w){1,15}/, message: "%{value} is not a valid twitter user" }
